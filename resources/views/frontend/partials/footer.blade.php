a3c32c85c629adb83e3d67ce8cbf4bf78f84cc61
andypratama3 <andypratama1211@gmail.com>
2024-04-23 22:49:10 +0800

    footer

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -2,9 +2,9 @@
     <div class="row gy-4">
       <div class="col-lg-5 col-md-12 footer-about">
         <a href="index.html" class="logo d-flex align-items-center">
-          <span>Append</span>
+          <span>TRGS</span>
         </a>
-        <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+        {{-- <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p> --}}
         <div class="social-links d-flex mt-4">
           <a href=""><i class="bi bi-twitter"></i></a>
           <a href=""><i class="bi bi-facebook"></i></a>
